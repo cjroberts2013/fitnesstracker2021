3191f4c35b44827cfce05df72a4e5f8aa0f5d1b1
CJ Roberts <cjroberts2013@gmail.com>
2020-12-19 01:30:49 -0500

    Progress setting up the header/navigation default. Still need to figure out why my css isnt working

---
+++ application/views/pages/about.php
@@ -0,0 +1,1 @@
+<h1 class="text-3xl text-black pb-6"><?php echo $title ?></h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Poppins', 'system-ui', 'sans-serif'],
+      body: ['Poppins', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      empty: {
+        emerald: '#10b981'
+      },
+      empty: {
+        lime: '#bef264'
+      }
+    },
     extend: {},
   },
   variants: {
